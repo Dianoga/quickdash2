831a7c132a43e841e14ffa7974f5bd191858a9ce
Brian Steere <dianoga7@3dgo.net>
2020-04-26 11:24:05 -0500

    feat(utils): Add ObjectSet

--- packages/quickdash-react/src/store/device.slice.ts
+++ packages/quickdash-react/src/store/device.slice.ts
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 import client from '../libs/feathers';
+import { extractCapabilities, ObjectSet } from '../utils/helpers';
 
 export const fetchDevices = createAsyncThunk(
 	'device/fetchDevices',
@@ -10,11 +11,13 @@ export const fetchDevices = createAsyncThunk(
 	}
 );
 
+export type Capability = { id: string; version: string };
+
 export type DeviceData = {
 	components: {
 		id: string;
 		label: string;
-		capabilities: { id: string; version: string }[];
+		capabilities: Capability[];
 	}[];
 	deviceId: string;
 	displayName: string;
@@ -27,11 +30,12 @@ export type DeviceData = {
 type SliceState = {
 	loading: boolean;
 	devices: { [deviceId: string]: DeviceData };
+	capabilities: Capability[];
 };
 
 const deviceSlice = createSlice({
 	name: 'device',
-	initialState: { loading: false, devices: {} } as SliceState,
+	initialState: { loading: false, devices: {}, capabilities: [] } as SliceState,
 	reducers: {},
 	extraReducers: (builder) => {
 		builder.addCase(fetchDevices.pending, (state) => {
@@ -46,10 +50,20 @@ const deviceSlice = createSlice({
 			const devices = action.payload;
 			const seenIds: string[] = [];
 
+			const capabilities = new ObjectSet(state.capabilities);
+
 			devices.forEach((d: DeviceData) => {
 				state.devices[d.deviceId] = d;
 				seenIds.push(d.deviceId);
+
+				extractCapabilities(d).forEach((c) => capabilities.add(c));
 			});
+
+			state.capabilities = Array.from(capabilities).sort(
+				(a: Capability, b: Capability) => {
+					return a.id <= b.id ? -1 : 1;
+				}
+			);
 
 			Object.keys(state.devices).forEach((key) => {
 				if (!seenIds.includes(key)) delete state.devices[key];
--- packages/quickdash-react/src/utils/helpers.ts
+++ packages/quickdash-react/src/utils/helpers.ts
@@ -1,3 +1,6 @@
+import { DeviceData, Capability } from '../store/device.slice';
+import { shallowEqual } from 'react-redux';
+
 export const extractDeviceComponentId = (id: DeviceComponentId) => {
 	const [deviceId, componentId] = id.split(':');
 
@@ -20,3 +23,34 @@ export const createWidgetId = (widgets: WidgetData[]) => {
 
 	return result;
 };
+
+export const extractCapabilities = (device: DeviceData) => {
+	const capabilities: Capability[] = [];
+	device.components.forEach((component) =>
+		component.capabilities.forEach((capability) =>
+			capabilities.push(capability)
+		)
+	);
+
+	return capabilities;
+};
+
+export class ObjectSet<T> extends Set {
+	constructor(items: T[] = []) {
+		super(items);
+	}
+
+	add(item: T) {
+		if (!this.has(item)) super.add(item);
+		return this;
+	}
+
+	has(item: T) {
+		let found = false;
+		this.forEach((i) => {
+			if (shallowEqual(i, item)) found = true;
+		});
+
+		return found;
+	}
+}
